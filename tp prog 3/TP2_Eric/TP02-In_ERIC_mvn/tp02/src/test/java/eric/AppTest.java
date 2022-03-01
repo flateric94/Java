b16f5e758b8e14446d717b33bcae437620cc287e
flateric94 <flateric94@github.com>
2022-03-01 22:14:20 +0100

    au dodo

--- tp prog 3/TP2_Eric/TP02-In_ERIC_mvn/tp02/src/test/java/eric/AppTest.java
+++ tp prog 3/TP2_Eric/TP02-In_ERIC_mvn/tp02/src/test/java/eric/AppTest.java
@@ -1,8 +1,9 @@
 package eric;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -12,9 +13,15 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    // @Test
+    // public void shouldAnswerWithTrue()
+    // {
+    //     assertTrue( true );
+    // }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testEricAvecAssertj()
     {
-        assertTrue( true );
+        assertThat(true).isTrue();
     }
 }
